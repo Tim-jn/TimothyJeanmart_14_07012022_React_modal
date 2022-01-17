7a783f379c76aa0daa5a6c41b775d51f503ee3a4
Tim <Tim280696@hotmail.com>
2022-01-17 14:56:56 +0100

    added comments

--- dist/components/Modal.js
+++ dist/components/Modal.js
@@ -9,16 +9,25 @@ require("core-js/modules/web.dom-collections.iterator.js");
 
 var _react = _interopRequireWildcard(require("react"));
 
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
 require("./styles/Modal.css");
 
 var _jsxRuntime = require("react/jsx-runtime");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+/**
+ * @param {*} props
+ * @returns {HTMLElement}
+ */
 function Modal(props) {
-  const [toggle, setToggle] = (0, _react.useState)(true);
+  // Set the display of the modal 
+  const [toggle, setToggle] = (0, _react.useState)(true); // Pass a function as props to close the modal in the main component
 
   const handleUserClick = () => {
     props.handleResponse();
@@ -42,3 +51,8 @@ function Modal(props) {
     }) : ''
   });
 }
+
+Modal.propTypes = {
+  text: _propTypes.default.string,
+  handleResponse: _propTypes.default.func
+};
--- src/lib/components/Modal.js
+++ src/lib/components/Modal.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
+import PropTypes from 'prop-types';
 import './styles/Modal.css'
 
+/**
+ * @param {*} props
+ * @returns {HTMLElement}
+ */
 export default function Modal(props) {
+
+// Set the display of the modal 
   const [toggle, setToggle] = useState(true)
 
+// Pass a function as props to close the modal in the main component
   const handleUserClick = () => {
     props.handleResponse()
   }
@@ -32,4 +40,9 @@ export default function Modal(props) {
       )}
     </div>
   )
+}
+
+Modal.propTypes = {
+  text: PropTypes.string,
+  handleResponse: PropTypes.func
 }
